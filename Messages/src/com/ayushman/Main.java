a1094ad3f030a1419bd573e5e51e321b46278fb7
Ayushman Sachan <ashusachan01@gmail.com>
2020-11-30 10:58:36 +0530

    Created Producer&Consumer-styled messaging to illustrate wait() and notifyAll()

--- Messages/src/com/ayushman/Main.java
+++ Messages/src/com/ayushman/Main.java
@@ -1,9 +1,13 @@
 package com.ayushman;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
     }
 }
 
@@ -14,31 +18,102 @@ class Message {
 
     public synchronized String read() {
         while(empty) {
+            try {
+                wait();
+            } catch(InterruptedException e) {
 
+            }
         }
         empty = true;
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message) {
 
         while(!empty) {
+            try {
+                wait();
+            } catch(InterruptedException e) {
 
+            }
         }
         empty = false;
         this.message = message;
+        notifyAll();
     }
 }
 
+
+//Producer class
 class Writer implements Runnable{
 
     private Message message;
 
-    public Writer (Message message) {
+    public Writer(Message message) {
         this.message = message;
     }
 
     public void run() {
-        String[] messages = {"Humpty Dumpty sat on a wall", "Humpty Dumpty had a great fall", "All the King's horses and all the King's men", "Couldn't put Humpty together again"};
+        String[] messages = {
+                "Humpty Dumpty sat on a wall",
+                "Humpty Dumpty had a great fall",
+                "All the King's horses and all the King's men",
+                "Couldn't put Humpty together again"
+        };
+
+        Random random = new Random();
+
+        for (String s : messages) {
+            message.write(s);
+
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+
+            }
+        }
+        message.write("Finished.");
     }
 }
+
+
+
+//Consumer class
+class Reader implements Runnable {
+
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished."); latestMessage = message.read()) {
+            System.out.println(latestMessage);
+
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
