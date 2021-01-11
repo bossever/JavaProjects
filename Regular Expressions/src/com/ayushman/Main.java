40d603caf31846e06705d0f9a4679045ffe186d2
Ayushman Sachan <ashusachan01@gmail.com>
2021-01-11 15:48:56 +0530

    Added more examples of Regular Expressions

--- Regular Expressions/src/com/ayushman/Main.java
+++ Regular Expressions/src/com/ayushman/Main.java
@@ -16,8 +16,15 @@ public class Main {
         System.out.println(alphaNumeric.replaceAll("ijkl99z$", "THE END")); //$ matches the end of a string, comes after the regex pattern
 
         System.out.println(alphaNumeric.replaceAll("[aei]", "X"));          //replaces all occurrences of a, e, or i with X
+        System.out.println(alphaNumeric.replaceAll("[aei][Fj]", "X"));      //replaces two letters a, e or i, followed by the letter F or j
+        System.out.println(alphaNumeric.matches("^abcDeee"));                           //returns false because the matches() method tries to match the whole string with the regex
 
-        System.out.println(alphaNumeric.matches("^abcDeee")); //returns false because the matches() method tries to match the whole string with the regex
+        System.out.println("Harry can also be written as harry".replaceAll("[Hh]arry", "Harry"));   //replaces harry starting with an upper or lower case h with "Harry"
 
+        System.out.println(alphaNumeric.replaceAll("[^ej]", "X"));
+        //a caret can be used a boundary matcher as well as a character class (in the example above) when used inside []
+
+        System.out.println(alphaNumeric.replaceAll("[a-f3-8]", "X"));
+        //replaces all characters that fall in the range specified (inclusive)
     }
 }
