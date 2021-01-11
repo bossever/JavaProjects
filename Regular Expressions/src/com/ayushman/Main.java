80b315c4e5965d65010398cecb9cc6a87c12b096
Ayushman Sachan <ashusachan01@gmail.com>
2021-01-11 17:09:05 +0530

    Added more examples for regex

--- Regular Expressions/src/com/ayushman/Main.java
+++ Regular Expressions/src/com/ayushman/Main.java
@@ -10,21 +10,70 @@ public class Main {
         System.out.println(yourString);
 
         String alphaNumeric = "abcDeeeF12Ghhiiiijkl99z";
-        System.out.println(alphaNumeric.replaceAll(".", "Y"));  //. is the character class that matches all characters, replaces all with 'Y'
 
-        System.out.println(alphaNumeric.replaceAll("^abcDeee", "YYY"));     //^ matches the beginning of a string, precedes the regex pattern
-        System.out.println(alphaNumeric.replaceAll("ijkl99z$", "THE END")); //$ matches the end of a string, comes after the regex pattern
+        //. is the character class that matches all characters, replaces all with 'Y'
+        System.out.println(alphaNumeric.replaceAll(".", "Y"));
 
-        System.out.println(alphaNumeric.replaceAll("[aei]", "X"));          //replaces all occurrences of a, e, or i with X
-        System.out.println(alphaNumeric.replaceAll("[aei][Fj]", "X"));      //replaces two letters a, e or i, followed by the letter F or j
-        System.out.println(alphaNumeric.matches("^abcDeee"));                           //returns false because the matches() method tries to match the whole string with the regex
+        //^ matches the beginning of a string, precedes the regex pattern
+        System.out.println(alphaNumeric.replaceAll("^abcDeee", "YYY"));
+        //$ matches the end of a string, comes after the regex pattern
+        System.out.println(alphaNumeric.replaceAll("ijkl99z$", "THE END"));
 
-        System.out.println("Harry can also be written as harry".replaceAll("[Hh]arry", "Harry"));   //replaces harry starting with an upper or lower case h with "Harry"
+        //replaces all occurrences of a, e, or i with X
+        System.out.println(alphaNumeric.replaceAll("[aei]", "X"));
+        //replaces two letters a, e or i, followed by the letter F or j
+        System.out.println(alphaNumeric.replaceAll("[aei][Fj]", "X"));
 
-        System.out.println(alphaNumeric.replaceAll("[^ej]", "X"));
+        //returns false because the matches() method tries to match the whole string with the regex
+        System.out.println(alphaNumeric.matches("^abcDeee"));
+
+        //replaces harry starting with an upper or lower case h with "Harry"
+        System.out.println("Harry can also be written as harry".replaceAll("[Hh]arry", "Harry"));
+
         //a caret can be used a boundary matcher as well as a character class (in the example above) when used inside []
+        System.out.println(alphaNumeric.replaceAll("[^ej]", "X"));
 
-        System.out.println(alphaNumeric.replaceAll("[a-f3-8]", "X"));
         //replaces all characters that fall in the range specified (inclusive)
+        System.out.println(alphaNumeric.replaceAll("[a-f3-8]", "X"));
+
+        //replaces all characters that fall in the range specified (inclusive), but with case sensitivity turned off
+        System.out.println(alphaNumeric.replaceAll("(?i)[a-f3-8]", "X"));
+        /*
+        (?i) --> ASCII strings
+        (?iu) --> UNICODE strings
+         */
+
+        //the below two statements achieve the same purpose of replacing all numbers in the string with X
+        System.out.println(alphaNumeric.replaceAll("[0-9]", "X"));
+        System.out.println(alphaNumeric.replaceAll("\\d", "X"));
+
+        //replace all non-digits with X
+        System.out.println(alphaNumeric.replaceAll("\\D", "X"));
+
+        String hasWhiteSpace = "I have blanks, a\ttab, and also, a \nnewline.";
+        System.out.println(hasWhiteSpace);
+
+        //replaces all whitespaces in the string with "" (nothing)
+        System.out.println(hasWhiteSpace.replaceAll("\\s", ""));
+        //replaces all non-whitespaces in the string with "" (nothing)
+        System.out.println(hasWhiteSpace.replaceAll("\\s", ""));
+        /*
+        \\s     --> match whitespace characters
+        \\S     --> match all non-whitespace characters
+
+        \\s     --> match a single whitespace character
+        \\s+    --> match sequence of one or more than one whitespace character
+
+        These two behave differently when the string is replaced with a non-empty string
+         */
+
+        //replaces all word characters with X
+        System.out.println(hasWhiteSpace.replaceAll("\\w", "X"));
+        /*
+            \w stands for "word character" --> [A-Za-z0-9_]
+         */
+
+
     }
+
 }
